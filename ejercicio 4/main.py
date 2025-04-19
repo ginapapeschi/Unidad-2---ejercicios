f8136f59d99c4f5fc8c7286470311810247efb79
ginapapeschi <166958436+ginapapeschi@users.noreply.github.com>
2025-04-19 18:48:08 -0300

    Delete ejercicio 4 directory

--- ejercicio 4/main.py
+++
@@ -1,8 +0,0 @@
-from Moto.clasemoto import Moto
-from Moto.gestorMoto import gestor_m
-from Pedido.clasepedido import Pedido
-from Pedido.gestorPedido import gestor_p
-
-if __name__ == '__main__':
-    def menu():
-        
